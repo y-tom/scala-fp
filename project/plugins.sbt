ef2672f90081cb3b2ee61114ba82666c06ab75f7
y-tom <114702943+y-tom@users.noreply.github.com>
2026-04-09 08:45:26 +0900

    Scala文法の落穂拾い

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,4 +3,4 @@ logLevel := Level.Warn
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.4.4")
 
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.6")
-addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.14.4")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.14.6")
---
+++ src/main/scala/UnapplyStudy.scala
@@ -0,0 +1,43 @@
+import scala.util.Try
+
+object UnapplyStudy extends App {
+
+  class User(private val name: String, private val age: Int)
+
+  // --- unapplyメソッドの実装 ---
+  object User {
+    //def unapply(user: User): Option[(String, Int)] = Some(user.name, user.age)
+    def unapply(obj: Any): Option[(String, Int)] = {
+      // --- obj が User 型であった時の実装 ---
+      // isInstanceOfメソッドで型チェック、asInstanceOfメソッドでUser型として取得して値を返す
+      if (obj.isInstanceOf[User]) {
+        val user = obj.asInstanceOf[User]
+        Some((user.name, user.age))
+      } else if (obj.isInstanceOf[String]) {
+        // 名前@年齢」のようになっている文字列を User とみなす
+        val strs = obj.asInstanceOf[String].split("@")
+        val name = strs.headOption
+        val age = Try {
+          strs.tail.headOption.map(_.toInt)
+        }.toOption.flatten
+        (name, age) match {
+          case (Some(n), Some(a)) => Some((n, a))
+          case _ => None
+        }
+      } else {
+        None
+      }
+    }
+  }
+
+  // --- パターンマッチで利用 ---
+  def printPatternMatched(obj: AnyRef): Unit = {
+    obj match {
+      case User(name, age) => println(s"Name: ${name}, Age: ${age}")
+      case _ => println("can't extract")
+    }
+  }
+
+  // printPatternMatched(new User("Taro", 17))
+  printPatternMatched("Jiro@16")
+}
